d294e6f20682042d900edd8d49f3bd66dd435c2c
Caleb Everett <everett.caleb@gmail.com>
2019-12-08 13:59:09 -0800

    Day 5

--- src/bin/intcode.rs
+++ src/bin/intcode.rs
@@ -11,51 +11,108 @@ fn main() -> io::Result<()> {
         .map(|i| i.parse::<i32>().unwrap())
         .collect();
 
-    for noun in 0..99 {
-        for verb in 0..99 {
-            let mut program = initial.clone();
-            program[1] = noun;
-            program[2] = verb;
-            program = run(program);
-            if program[0] == 19690720 {
-                println!("{}{}", noun, verb);
-            }
-        }
-    }
+    println!("part 1");
+    let program = Program { memory: initial.clone(), counter: 0 };
+    program.run(1);
+
+    println!("part 2");
+    let program = Program { memory: initial.clone(), counter: 0 };
+    program.run(5);
     Ok(())
 }
 
-fn run(mut program: Vec<i32>) -> Vec<i32> {
-    let mut counter = 0;
+struct Program {
+    counter: usize,
+    memory: Vec<i32>,
+}
 
-    loop {
-        match program[counter] {
-            1 => {
-                // add
-                let a_index = program[counter+1];
-                let b_index = program[counter+2];
-                let c_index = program[counter+3];
-                program[c_index as usize] = program[a_index as usize] + program[b_index as usize];
-                counter+=4;
-            }
-            2 => {
-                // mult
-                let a_index = program[counter+1];
-                let b_index = program[counter+2];
-                let c_index = program[counter+3];
-                program[c_index as usize] = program[a_index as usize] * program[b_index as usize];
-                counter+=4;
-            }
-            99 => {
-                // halt
-                break;
-            }
-            _ => {
-                panic!("encounted unexpected code");
-            }
+impl Program {
+    fn read_opcode(&mut self) -> (i32, i32) {
+        let opcode = self.memory[self.counter] % 100;
+        let paramode = self.memory[self.counter] / 100;
+        self.counter+=1;
+        (opcode, paramode)
+    }
+
+    fn read_params(&mut self, l: usize, mut paramode: i32) -> Vec<usize> {
+        let mut vec = Vec::with_capacity(l);
+        for _ in 0..l {
+            let mode = paramode % 10;
+            paramode = paramode / 10;
+            let idx =  match mode {
+                1 => self.counter,
+                0 => self.memory[self.counter] as usize,
+                _ => panic!("unknown paramode {}", paramode),
+            };
+            vec.push(idx);
+            self.counter+=1;
+        }
+        vec
+    }
+
+    fn run(mut self, input: i32) {
+        loop {
+            let (opcode, paramode) = self.read_opcode();
+            match opcode {
+                1 => {
+                    // add 2=0+1
+                    let params = self.read_params(3, paramode);
+                    self.memory[params[2]] = self.memory[params[0]] + self.memory[params[1]];
+                },
+                2 => {
+                    // mul 2=0*1
+                    let params = self.read_params(3, paramode);
+                    self.memory[params[2]] = self.memory[params[0]] * self.memory[params[1]];
+                },
+                3 => {
+                    // input 0=input
+                    let params = self.read_params(1, paramode);
+                    self.memory[params[0] as usize] = input;
+                },
+                4 => {
+                    // output output=0
+                    let params = self.read_params(1, paramode);
+                    println!("{}", self.memory[params[0]]);
+                },
+                5 => {
+                    // jump if false 0
+                    let params = self.read_params(2, paramode);
+                    if self.memory[params[0]] != 0 {
+                        self.counter = self.memory[params[1]] as usize;
+                    }
+                },
+                6 => {
+                    // jump if true 0
+                    let params = self.read_params(2, paramode);
+                    if self.memory[params[0]] == 0 {
+                        self.counter = self.memory[params[1]] as usize;
+                    }
+                },
+                7 => {
+                    // less than 2 = 0<1 ? 1 : 0
+                    let params = self.read_params(3, paramode);
+                    self.memory[params[2]] = if self.memory[params[0]] < self.memory[params[1]] {
+                        1
+                    } else {
+                        0
+                    }
+                },
+                8 => {
+                    // equals 2 = 0==1 ? 1  : 0
+                    let params = self.read_params(3, paramode);
+                    self.memory[params[2]] = if self.memory[params[0]] == self.memory[params[1]] {
+                        1
+                    } else {
+                        0
+                    }
+                },
+                99 => {
+                    break;
+                },
+                _ => panic!("unknown opcode {}", opcode),
+            };
         }
     }
-    program
 }
 
 #[derive(StructOpt)]
